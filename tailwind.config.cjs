2c07338131acc283e97e6aab79c824838f2ced02
Danesh Tolani <100458159+danesh-tolani@users.noreply.github.com>
2023-03-21 18:31:36 +0000

    added tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      primary: "Poppins",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1440px"
+    },
+    extend: {
+      colors: {
+        primary: "#222222",
+        secondary: "#F5E6E0"
+      }
+    },
+  },
+  plugins: [],
+}
